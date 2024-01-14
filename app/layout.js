ed1da343099f8b6387a917079e36ab4ba6ae1ed4
satviktripathi369 <satviktripathi2003@gmail.com>
2024-01-14 14:47:03 +0530

    new app developed but oauth not working fixing that

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Provider from './context/AuthContext'
+import ToasterContent from './context/ToasterContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}><Provider><ToasterContent />{children}</Provider></body>
     </html>
   )
 }
